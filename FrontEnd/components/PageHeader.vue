df8273f2c5d17df95fb53de42b2f3c9c7e633b02
User201114 <2859648675@qq.com>
2021-09-28 18:38:52 +0800

    add docters, masters page

--- FrontEnd/components/PageHeader.vue
+++ FrontEnd/components/PageHeader.vue
@@ -1,3 +1,10 @@
+<!--
+ * @FileDescription: header page
+ * @Author: wenbin, liuyoude
+ * @Date: 2021-09-28
+ * @LastEditors: liuyoude
+ * @LastEditTime: 2021-09-28
+ -->
 <template>
   <div class="page-title-area bg_cover pt-120" style="background-image: url(/images/banner-bg-1.jpg);">
     <div class="container">
@@ -6,11 +13,11 @@
           <div class="page-title-item text-center">
             <nav aria-label="breadcrumb">
               <ol class="breadcrumb">
-                <li class="breadcrumb-item"><nuxt-link to="/">首页</nuxt-link></li>
-                <li class="breadcrumb-item active" aria-current="page">{{ title }}</li>
+<!--                <li class="breadcrumb-item"><nuxt-link to="/">首页</nuxt-link></li>-->
+                <li class="breadcrumb-item active" aria-current="page">{{ title_en }}</li>
               </ol>
             </nav>
-            <h3 class="title">{{ title }}</h3>
+            <h3 class="title">{{ title_zh }}</h3>
           </div>
         </div>
       </div>
@@ -25,10 +32,15 @@
   export default {
     name: "PageHeader",
     props: {
-      title: {
+      title_zh: {
         type: String
+      },
+      title_en: {
+          type: String
       }
     },
+
+
   }
 </script>
 
