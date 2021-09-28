16347cace456dae2d0024272f564aa73f02f9e97
zxy <1501434265@qq.com>
2021-09-28 15:08:21 +0800

    修改models

--- BackEnd/API/models.py
+++ BackEnd/API/models.py
@@ -4,21 +4,6 @@ from django.db import models
 from django.db import models
 
 
-class Users(models.Model):
-    """
-    用户
-    """
-    name = models.CharField(max_length=30, null=True, blank=True, verbose_name="姓名")
-    password = models.CharField(max_length=30, null=True, blank=True, verbose_name="密码")
-    birthday = models.DateField(null=True, blank=True, verbose_name="出生年月")
-    gender = models.CharField(max_length=6, choices=(("male", "男"), ("female", "女")), default="female", verbose_name="性别")
-    mobile = models.CharField(null=True, blank=True, max_length=11, verbose_name="电话")
-    email = models.EmailField(max_length=100, null=True, blank=True, verbose_name="邮箱")
-
-    class Meta:
-        verbose_name = "用户"
-        verbose_name_plural = verbose_name
-
 
 class News(models.Model):
 
