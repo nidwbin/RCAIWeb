059f29d631f2497bc5c2ed0b5f78ec8cbf610ff2
nidwbin <491216697@qq.com>
2021-10-31 19:11:58 +0800

    打包环境

--- FrontEnd/run.sh
+++ FrontEnd/run.sh
@@ -1,5 +1,3 @@
 # !/bin/bash
-npm config set proxy socks5://192.168.11.211:10808
-npm config set https-proxy socks5://192.168.11.211:10808
-npm install
+tar -zxvf env.tar.gz
 npm run build && npm run start
